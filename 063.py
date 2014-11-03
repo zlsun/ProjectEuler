59c27e61e879054c5349b42ac5ca9c2fa5f460aa
zlsun <zlsun1995@gmail.com>
2014-11-03 17:18:52 +0800

    Solve #063, #080

--- 063.py
+++ 063.py
@@ -2,11 +2,11 @@
 """
 Powerful digit counts
 
-The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
 How many n-digit positive integers exist which are also an nth power?
 """
 
 from utils import *
 
-
-# 
+print len([(i, n) for n in range(1, 200) for i in range(1, 10) if len(str(i ** n)) == n])
+# 49
--- 080.py
+++ 080.py
@@ -7,7 +7,20 @@ The square root of two is 1.41421356237309504880..., and the digital sum of the 
 For the first one hundred natural numbers, find the total of the digital sums of the first one hundred decimal digits for all the irrational square roots.
 """
 
+from decimal import *
+
 from utils import *
 
+getcontext().prec = 102
+p = 10 ** 100
 
-# 
+
+def sum_of_sqrt_decimal(s):
+    q = Decimal(s).sqrt()
+    if q % 1 == 0:
+        return 0
+    return sum(map(int, str(q * p)[:100]))
+
+print sum(sum_of_sqrt_decimal(str(i)) for i in range(1, 101))
+
+# 40886
