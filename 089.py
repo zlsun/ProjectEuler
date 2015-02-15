08106546cb27ed66f5e6e2eb77b9153c23307d39
zlsun <zlsun1995@gmail.com>
2015-02-15 23:41:19 +0800

    Solve #089

--- 089.py
+++ 089.py
@@ -21,4 +21,42 @@ Note: You can assume that all the Roman numerals in the file contain no more tha
 from utils import *
 
 
-# 
+def to_roman(i):
+    s = ''
+    while i >= 1000:
+        s += 'M'
+        i -= 1000
+    for m, b, c, d in [(100, 'C', 'D', 'M'), (10, 'X', 'L', 'C'), (1, 'I', 'V', 'X')]:
+        n = i / m
+        if n < 4:
+            s += b * n
+        elif n == 4:
+            s += b + c
+        elif n < 9:
+            s += c + b * (n - 5)
+        elif n == 9:
+            s += b + d
+        i %= m
+    return s
+
+
+def from_roman(s):
+    sp = {'I': 'VX', 'X': 'LC', 'C': 'DM'}
+    c2i = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r = 0
+    i = 0
+    l = len(s)
+    while i < l:
+        c = s[i]
+        if c in sp.keys() and i + 1 < l and s[i + 1] in sp[c]:
+            r += c2i[s[i + 1]] - c2i[c]
+            i += 1
+        else:
+            r += c2i[c]
+        i += 1
+    return r
+
+
+L = file('./089_roman.txt').read().splitlines()
+print sum(len(l) - len(to_roman(from_roman(l))) for l in L)
+# 743
